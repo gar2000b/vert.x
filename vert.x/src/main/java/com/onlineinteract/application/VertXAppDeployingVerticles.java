265a812cac7d8721cc94ede1a5e56d2a36076178
Gary Black <gar2000b@yahoo.com>
2016-03-26 22:23:11 +0000

    2. enhancing our vertcle deployment and un-deployment to gain further understanding. Documented the three main types of verticles. Using standard verticle type in this example.

--- vert.x/src/main/java/com/onlineinteract/application/VertXAppDeployingVerticles.java
+++ vert.x/src/main/java/com/onlineinteract/application/VertXAppDeployingVerticles.java
@@ -7,19 +7,54 @@ import io.vertx.core.VertxOptions;
 
 public class VertXAppDeployingVerticles {
 
+	private String deploymentID;
+	private Vertx vertx;
+
 	public VertXAppDeployingVerticles() {
-		Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(40));
-		vertx.deployVerticle(new MyVerticle());
+		vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(40));
+
+		// deploying a standard verticle.
+		vertx.deployVerticle(new MyVerticle(), res -> {
+			if (res.succeeded()) {
+				deploymentID = res.result();
+				System.out.println("Deployment id is: " + res.result());
+			} else {
+				System.out.println("Deployment failed!");
+			}
+		});
+
+		// can also deploy verticle using it's name. In java this is the fqn of
+		// the class.
+		// vertx.deployVerticle("com.onlineinteract.verticles.MyVerticle");
 	}
-	
+
+	private void undeployVerticles() {
+		System.out.println("Do we even get here");
+		vertx.undeploy(deploymentID, res -> {
+			System.out.println("* undeploying verticles");
+			if (res.succeeded()) {
+				System.out.println("Undeployed ok");
+			} else {
+				System.out.println("Undeploy failed!");
+			}
+		});
+	}
+
 	private String someMethod() {
 		return "Processed some data independent of other vert.x Threads.";
 	}
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Running...");
-		VertXAppDeployingVerticles verticles = new VertXAppDeployingVerticles();
-		String result = verticles.someMethod();
+		VertXAppDeployingVerticles vertXAppDeployingVerticles = new VertXAppDeployingVerticles();
+		String result = vertXAppDeployingVerticles.someMethod();
 		System.out.println(result);
+		
+		// The following allows us to undeploy before our verticle completes and undeploys first.
+		// albeit, our verticle kicks off a blocking future which will complete OK even if we
+		// undeploy the vertcle beforehand.
+		Thread.sleep(3000);
+		vertXAppDeployingVerticles.undeployVerticles();
+		Thread.sleep(1000);
 	}
 }
--- vert.x/src/main/java/com/onlineinteract/verticles/MyVerticle.java
+++ vert.x/src/main/java/com/onlineinteract/verticles/MyVerticle.java
@@ -7,6 +7,21 @@ import com.onlineinteract.thread.CountDownLatchDecrementer;
 import io.vertx.core.AbstractVerticle;
 
 public class MyVerticle extends AbstractVerticle {
+	
+/*	Verticle Types
+	There are three different types of verticles:
+
+	Standard Verticles
+	These are the most common and useful type - they are always executed 
+	using an event loop thread. We’ll discuss this more in the next section.
+
+	Worker Verticles
+	These run using a thread from the worker pool. An instance is never 
+	executed concurrently by more than one thread.
+
+	Multi-threaded worker verticles
+	These run using a thread from the worker pool. An instance can be 
+	executed concurrently by more than one thread. */
 
 	// Called when verticle is deployed
 	public void start() {
